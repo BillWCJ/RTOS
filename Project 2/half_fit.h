6bad03cbe081d54c94a6a5f06df9231657cbb3aa
BillWenChaoJiang <jiang.bill@hotmail.com>
2015-10-05 16:34:11 -0400

    Add proj 2

---
+++ Project 2/half_fit.h
@@ -0,0 +1,15 @@
+#ifndef HALF_FIT_H_
+#define HALF_FIT_H_
+
+/*
+ * Author names:
+ *   1.  uWaterloo User ID:  xxxxxxxx@uwaterloo.ca
+ *   2.  uWaterloo User ID:  xxxxxxxx@uwaterloo.ca
+ */
+
+void  half_init( void );
+void *half_alloc( int );
+// or void *half_alloc( unsigned int );
+void  half_free( void * );
+
+#endif
