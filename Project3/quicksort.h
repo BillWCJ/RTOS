6b8f0f3046612b48b9d5755decb6bac3a45ab939
BillWenChaoJiang <jiang.bill@hotmail.com>
2015-11-03 17:22:40 -0500

    Add proj 3 files

---
+++ Project3/quicksort.h
@@ -0,0 +1,16 @@
+#ifndef __QUICKSORT_H__
+#define __QUICKSORT_H__
+
+/*
+ * Author names:
+ *   1.  uWaterloo User ID:  xxxxxxxx@uwaterloo.ca
+ *   2.  uWaterloo User ID:  xxxxxxxx@uwaterloo.ca
+ */
+
+#include "array_tools.h"
+
+void quicksort( array_t );
+// Uncomment this when you implement Project 4
+// void quicksort_sem( array_t );
+
+#endif
