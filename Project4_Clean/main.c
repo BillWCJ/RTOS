81cdfa67c5dda3dbfc4f59bf3d4088a7e4df08f3
Pavel Shering <pavel.shering@hotmail.ca>
2015-11-25 20:55:37 -0500

    fixed for loops for the LED tracking

--- Project4_Clean/main.c
+++ Project4_Clean/main.c
@@ -38,26 +38,65 @@ OS_SEM Rand;
 OS_SEM LEDCheck;
 
 unsigned short clear_bitmap[] = { BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
-																	BG, BG, BG, BG, BG, BG, BG, BG, BG, BG};
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, BG, BG, BG, BG};
 
 unsigned short cross_bitmap[] = { BG, BG, BG, PU, PU, PU, PU, BG, BG, BG,
-																	BG, BG, PU, PU, PU, PU, PU, PU, BG, BG,
-																	BG, PU, PU, PU, PU, PU, PU, PU, PU, BG,
-																	PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
-																	PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
-																	PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
-																	PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
-																	BG, PU, PU, PU, PU, PU, PU, PU, PU, BG,
-																	BG, BG, PU, PU, PU, PU, PU, PU, BG, BG,
-																	BG, BG, BG, PU, PU, PU, PU, BG, BG, BG};		
+								BG, BG, PU, PU, PU, PU, PU, PU, BG, BG,
+								BG, PU, PU, PU, PU, PU, PU, PU, PU, BG,
+								PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+								PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+								PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+								PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+								BG, PU, PU, PU, PU, PU, PU, PU, PU, BG,
+								BG, BG, PU, PU, PU, PU, PU, PU, BG, BG,
+								BG, BG, BG, PU, PU, PU, PU, BG, BG, BG};
+
+unsigned short mediumBall[] = { BG, BG, BG, BG, BG, BG, PU, PU, PU, BG, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG,
+								BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG,
+								BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG,
+								BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG,
+								PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+								PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+								PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+								BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG,
+								BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG,
+								BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG,
+								BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, BG, BG, BG, BG, BG,
+								BG, BG, BG, BG, BG, BG, PU, PU, PU, BG, BG, BG, BG, BG, BG };
+
+
+unsigned short largeBall[] = { BG, BG, BG, BG, BG, BG, BG, BG, PU, PU, PU, PU, BG, BG, BG, BG, BG, BG, BG, BG,
+							BG, BG, BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG, BG, BG, BG,
+							BG, BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG, BG, BG,
+							BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG, BG,
+							BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG,
+							BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG,
+							BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG,
+							BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG,
+							PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+							PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+							PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+							PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+							BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU,
+							BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG,
+							BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG,
+							BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG,
+							BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG,
+							BG, BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG, BG,
+							BG, BG, BG, BG, BG, BG, BG, PU, PU, PU, PU, PU, PU, BG, BG, BG, BG, BG, BG, BG,
+							BG, BG, BG, BG, BG, BG, BG, BG, PU, PU, PU, PU, BG, BG, BG, BG, BG, BG, BG, BG };
+
 const int size_x = 10, size_y = 10;
 
 volatile typedef struct {
@@ -67,14 +106,14 @@ volatile typedef struct {
 	volatile double speed_y;
 	volatile int size_x;
 	volatile int size_y;
-	volatile int color;
+	volatile unsigned short color;
 	volatile int radius;
 	volatile bool active;
-	volatile bool userControl; // decide later 
+	volatile bool userControl; // decide later
 } bounceBall;
 
 volatile bounceBall* ballParam[8];
-	
+
 
 // Stop blinking
 unsigned char stopBlink = 0;
@@ -83,13 +122,18 @@ unsigned char stopBlink = 0;
 //     http:// www.nxp.com/documents/user_manual/UM10360.pdf
 
 // Functions
+unsigned short getColour(void){
+	return (rand(sizeof(colourList)-1))
+}
+volatile colourList[] = {Black, Navy, DarkGreen, DarkCyan, Maroon, Purple, Olive, LightGrey, DarkGrey, Blue, Green, Cyan, Red, Magenta, Yellow}
+
 void position(bounceBall* ballParam){
 	os_sem_wait(&Rand, 0xFFFF);
 	ballParam->x = rand()%(MAXX-size_x);
 	ballParam->y = rand()%(MAXY-size_y);
 	ballParam->speed_x = (rand()%7) -3; // can only be from 0-6 then shifted to -3 to 3
 	ballParam->speed_y = (rand()%7) -3;
-	os_sem_send(&Rand);			
+	os_sem_send(&Rand);
 }
 
 void button_pressed_callback(void){
@@ -115,66 +159,66 @@ void draw_ball(bounceBall* ballParam, int old_x, int old_y, unsigned char*clear_
 	os_sem_send(&Draw);
 }
 
-void DrawCircle(int x0, int y0, int radius)
-{
-  int x = radius;
-  int y = 0;
-  int decisionOver2 = 1 - x;   // Decision criterion divided by 2 evaluated at x=r, y=0
-
-  while( y <= x )
-  {
-    GLCD_PutPixel( x + x0,  y + y0); // Octant 1
-    GLCD_PutPixel( y + x0,  x + y0); // Octant 2
-    GLCD_PutPixel(-x + x0,  y + y0); // Octant 4
-    GLCD_PutPixel(-y + x0,  x + y0); // Octant 3
-    GLCD_PutPixel(-x + x0, -y + y0); // Octant 5
-    GLCD_PutPixel(-y + x0, -x + y0); // Octant 6
-    GLCD_PutPixel( x + x0, -y + y0); // Octant 8
-    GLCD_PutPixel( y + x0, -x + y0); // Octant 7
-    y++;
-    if (decisionOver2<=0)
-    {
-      decisionOver2 += 2 * y + 1;   // Change in decision criterion for y -> y+1
-    }
-    else
-    {
-      x--;
-      decisionOver2 += 2 * (y - x) + 1;   // Change for y -> y+1, x -> x-1
-    }
-  }
-}
-
-void draw_ball2(int x, int y, int old_x, int old_y, int r, unsigned short colour){
-// 	int i, j;
-// 	double dx, dy;
-	os_sem_wait(&Draw, 0xFFFF);
-	// clear old circle
-	GLCD_SetTextColor(BG);
-	DrawCircle(old_x, old_y, r);
-// 	for (i = old_x-r; i<old_x+r; i++){
-// 		for (j = old_y-r; j<old_y+r; j++){
-// 			dx = i-old_x;
-// 			dy = j-old_y;
-// 			if((dx*dx + dy*dy) <= r*r)
-// 				GLCD_PutPixel(i, j);
-// 		}
-// 	}
-	
-	//draw circle
-	GLCD_SetTextColor(colour);
-// 	for (i = x - r; i < x+r; i++){
-// 		for (j = y - r; j < y+r; j++){
-// 			dx = i-x;
-// 			dy = j-y;
-// 			if((dx*dx + dy*dy) <= r*r)
-// 				GLCD_PutPixel(i, j);
-// 		}
-// 	}
-
-	DrawCircle(x, y, r);
-	os_sem_send(&Draw);
-}
- 
+// void DrawCircle(int x0, int y0, int radius)
+// {
+//   int x = radius;
+//   int y = 0;
+//   int decisionOver2 = 1 - x;   // Decision criterion divided by 2 evaluated at x=r, y=0
+//
+//   while( y <= x )
+//   {
+//     GLCD_PutPixel( x + x0,  y + y0); // Octant 1
+//     GLCD_PutPixel( y + x0,  x + y0); // Octant 2
+//     GLCD_PutPixel(-x + x0,  y + y0); // Octant 4
+//     GLCD_PutPixel(-y + x0,  x + y0); // Octant 3
+//     GLCD_PutPixel(-x + x0, -y + y0); // Octant 5
+//     GLCD_PutPixel(-y + x0, -x + y0); // Octant 6
+//     GLCD_PutPixel( x + x0, -y + y0); // Octant 8
+//     GLCD_PutPixel( y + x0, -x + y0); // Octant 7
+//     y++;
+//     if (decisionOver2<=0)
+//     {
+//       decisionOver2 += 2 * y + 1;   // Change in decision criterion for y -> y+1
+//     }
+//     else
+//     {
+//       x--;
+//       decisionOver2 += 2 * (y - x) + 1;   // Change for y -> y+1, x -> x-1
+//     }
+//   }
+// }
+//
+// void draw_ball2(int x, int y, int old_x, int old_y, int r, unsigned short colour){
+// // 	int i, j;
+// // 	double dx, dy;
+// 	os_sem_wait(&Draw, 0xFFFF);
+// 	// clear old circle
+// 	GLCD_SetTextColor(BG);
+// 	DrawCircle(old_x, old_y, r);
+// // 	for (i = old_x-r; i<old_x+r; i++){
+// // 		for (j = old_y-r; j<old_y+r; j++){
+// // 			dx = i-old_x;
+// // 			dy = j-old_y;
+// // 			if((dx*dx + dy*dy) <= r*r)
+// // 				GLCD_PutPixel(i, j);
+// // 		}
+// // 	}
+//
+// 	//draw circle
+// 	GLCD_SetTextColor(colour);
+// // 	for (i = x - r; i < x+r; i++){
+// // 		for (j = y - r; j < y+r; j++){
+// // 			dx = i-x;
+// // 			dy = j-y;
+// // 			if((dx*dx + dy*dy) <= r*r)
+// // 				GLCD_PutPixel(i, j);
+// // 		}
+// // 	}
+//
+// 	DrawCircle(x, y, r);
+// 	os_sem_send(&Draw);
+// }
+//
 // ################### TASKS ###################
 
 __task void ball_task(void* ballParam){
@@ -184,24 +228,21 @@ __task void ball_task(void* ballParam){
 	ball->y = 0;
 	ball->speed_x = 0;
 	ball->speed_y = 0;
-	
-	
 
 	position(ball);
-															
-							
+																			
 	while(1){
 		ball->x += ((double)adc/MAX_ADC)*ball->speed_x;
 		ball->y += ((double)adc/MAX_ADC)*ball->speed_y;
 		check_edge(ball, old_x, old_y);
-		
+
 		if(save_draw && (ball->x != old_x || ball->y != old_y)){
 			draw_ball(ball, old_x, old_y, (unsigned char*)clear_bitmap, (unsigned char*)cross_bitmap);
-			
+
 			//draw_ball2(x+(size_x/2), y+(size_y/2), old_x+(size_x/2), old_y+(size_y/2), ((size_x+size_y)/4), FG);
-			
+
 			old_x = ball->x;
-			old_y = ball->y;	
+			old_y = ball->y;
 		}
 	}
 	os_tsk_delete_self();
@@ -216,18 +257,18 @@ __task void user_task(void* ballParam){
 	ball->y = 0;
 	ball->speed_x = 0;
 	ball->speed_y = 0;
-	
-	
-																														
-	position(ball);										
-																			
+
+
+
+	position(ball);
+
 	while(1){
 		val = get_button();
-		
+
 		ball->x += ((double)adc/MAX_ADC)*ball->speed_x;
 		ball->y += ((double)adc/MAX_ADC)*ball->speed_y;
 		check_edge(ball, old_x, old_y);
-		
+
 		//DECIDE LATER accel?
 		if(val == KBD_UP && ball->y < (MAXY - size_y))
 				ball->speed_y += 1;
@@ -237,24 +278,24 @@ __task void user_task(void* ballParam){
 				ball->speed_y += -1;
 		else if(val == KBD_LEFT && ball->x > 0)
 				ball->speed_x += -1;
-		
+
 		if(ball->speed_x > 3)
 			ball->speed_x = 3;
 		else if(ball->speed_x < -3)
 			ball->speed_x = -3;
-		
+
 		if(ball->speed_y > 3)
 			ball->speed_y = 3;
 		else if(ball->speed_y < -3)
 			ball->speed_y = -3;
-			
+
 		if(save_draw && (ball->x != old_x || ball->y != old_y)){
 			draw_ball(ball, old_x, old_y, (unsigned char*)clear_bitmap, (unsigned char*)cross_bitmap);
-			
+
 			//draw_ball2(x+(size_x/2), y+(size_y/2), old_x+(size_x/2), old_y+(size_y/2), ((size_x+size_y)/4), FG);
-			
+
 			//draw_ball2(x+(size_x/2), y+(size_y/2), old_x+(size_x/2), old_y+(size_y/2), 10, FG);
-			
+
 			old_x = ball->x;
 			old_y = ball->y;
 		}
@@ -272,7 +313,7 @@ __task void readPoti_task(void){
 
 __task void ballCountLED(void){
 	int i;
-	for(i = 0; i < MAX_BALLS-1; i++){
+	for(i = 0; i < MAX_BALLS; i++){
 		if (ballParam[i]->active)
 			turnOnLED(i);
 		else
@@ -284,30 +325,27 @@ __task void init_task( void ) {
 	int i;
 	// Increase the priority to intilize the task first, then allow them to start
 	os_tsk_prio_self ( 2 );
-	
-	for( i = 0; i < 7; i++){
-		ballParam[i] = (bounceBall*)malloc(sizeof(bounceBall));
-		ballParam[i]->active = false;
-	}
-	
+
+
+
 	//initialize semaphores
 	os_sem_init(&Draw, 1);
 	os_sem_init(&Rand, 1);
 	os_sem_init(&Create, 1);
 	os_sem_init(&LEDCheck, 1);
-	
+
 	// Launch a multi thread checking system
 	os_tsk_create (readPoti_task, 1);
 	os_tsk_create (ballCountLED, 1);
-	
+
 	balls ++;
-	ballParam[0]->active = true;
+	ballParam[balls-1]->active = true;
 // 	turnOnLED(balls-1);
-	os_tsk_create_ex(user_task, 1, (void*)ballParam[0]);
+	os_tsk_create_ex(user_task, 1, (void*)ballParam[balls-1]);
 	os_tsk_prio_self ( 1 );
-	
+
 	while(1){
-		unsigned short int pot = 0; 
+		unsigned short int pot = 0;
 		if(create){
 			if(balls < 8){
 				os_sem_wait(&Draw, 0xFFFF);
@@ -317,7 +355,7 @@ __task void init_task( void ) {
 				balls++;
 				os_sem_send(&Draw);
 			}
-			else{	
+			else{
 				os_sem_wait(&Draw, 0xFFFF);
 				create = false;
 				os_sem_send(&Draw);
@@ -342,7 +380,14 @@ int main( void ) {
 	INT0Init(button_pressed_callback);
 	ADCInit();
 	KBD_Init();
-	
+
+	for( i = 0; i < 8; i++){
+		ballParam[i] = (bounceBall*)malloc(sizeof(bounceBall));
+		ballParam[i]->active = false;
+		ballParam[i]->size_x = 10;
+		ballParam[i]->size_y = 10;
+	}
+
 	os_sys_init(init_task);
 		while(1){
 	}
@@ -350,7 +395,7 @@ int main( void ) {
 
 // __task void blink_task( void *param ) {
 // 	unsigned char currentLED = *((unsigned char *) param);
-// 	
+//
 // 	// Pause until the next task is cleared. Although the priority in the init_task
 // 	// doesn't allow any blink_task to be started before the initializtion compltes,
 // 	// the below check makes the code safer.
@@ -377,9 +422,8 @@ int main( void ) {
 
 // 		// Call common signal function
 // 		os_evt_set  ( 0x0001, nextTaskArray[ currentLED ] );
-// 		
+//
 // 		//pass the control to the other thread.
 // 		os_tsk_pass();
 // 	}
 // }
-
